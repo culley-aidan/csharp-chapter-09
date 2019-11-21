ddc48f02eac25e1c226e6a1beaf171e67afe5b86
culley-aidan <culley-aidan@users.noreply.github.com>
2019-11-21 09:02:07 -0600

    finished

---
+++ CreateStudents/CreateStudents/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CreateStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student first = new Student();
+            Student second = new Student();
+            first.IdNumber = 123;
+            first.LastName = "Anderson";
+            first.GradePointAverage = 3.5;
+            second.IdNumber = 789;
+            second.LastName = "Daniels";
+            second.GradePointAverage = 4.1;
+            Display(first);
+            Display(second);
+        }
+        static void Display(Student stu)
+        {
+            Console.WriteLine("{0, 5} {1, -10}{2, 6}", stu.IdNumber, stu.LastName, stu.GradePointAverage.ToString("F1"));
+        }
+    }
+    class Student
+    {
+        private double gradePointAverage;
+        public const double HIGHEST_GPA = 4.0;
+        public const double LOWEST_GPA = 0.0;
+        public int IdNumber { get; set; }
+        public string LastName { get; set; }
+        public double GradePointAverage
+        {
+            get
+            {
+                return gradePointAverage;
+            }
+            set
+            {
+                if (value >= LOWEST_GPA && value <= HIGHEST_GPA)
+                {
+                    gradePointAverage = value;
+                }
+                else
+                {
+                    gradePointAverage = LOWEST_GPA;
+                }
+            }
+        }
+    }
+}
