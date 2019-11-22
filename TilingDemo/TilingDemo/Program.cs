562cff686560e969bace9c23b7db4919d91fc96f
culley-aidan <culley-aidan@users.noreply.github.com>
2019-11-22 09:24:05 -0600

    finished Room Class

--- TilingDemo/TilingDemo/Program.cs
+++ TilingDemo/TilingDemo/Program.cs
@@ -11,7 +11,29 @@ namespace TilingDemo
     }
     class Room
     {
-        public double length { get; set; }
-        public double width { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        private readonly double area;
+        private int boxes()
+        {
+            int returnValue = (int)area / 12;
+            if (area % 12 != 0)
+            {
+                ++returnValue;
+            }
+            ++returnValue;
+            return returnValue;
+        }
+        public int BoxTotal()
+        {
+            return boxes();
+        }
+        public Room(double length, double width)
+        {
+            Length = length;
+            Width = width;
+            area = length * width;
+        }
+        public Room() : this(0,0) { }
     }
 }
