55c1085be52093b519c41fff68f7fa1ba4a13a48
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-02 09:17:43 -0600

    started

---
+++ DemoJobs/DemoJobs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoJobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
