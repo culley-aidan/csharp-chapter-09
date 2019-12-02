399618d0a30a6712a620a45ed01acaac8058c2e3
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-02 09:05:07 -0600

    finished conference class

--- ConferencesDemo/ConferencesDemo/Program.cs
+++ ConferencesDemo/ConferencesDemo/Program.cs
@@ -9,4 +9,32 @@ namespace ConferencesDemo
             Console.WriteLine("Hello World!");
         }
     }
+    class Conference : IComparable
+    {
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public int Attendees { get; set; }
+        public int CompareTo(object obj)
+        {
+            if (obj == null) {
+                return 1;
+            }
+            Conference conference = obj as Conference;
+            if (conference != null)
+            {
+                return Attendees.CompareTo(conference.Attendees);
+            } else {
+                throw new ArgumentException("object is not a conference");
+            }
+        }
+
+        public Conference(string name, string date, int attendees)
+        {
+            Name = name;
+            Date = date;
+            Attendees = attendees;
+        }
+
+        public Conference() => new Conference("", "", 0);
+    }
 }
