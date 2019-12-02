6b5aa59cd4839caf16c88610a91b12d1066fba46
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-02 09:21:10 -0600

    started Job class

--- DemoJobs/DemoJobs/Program.cs
+++ DemoJobs/DemoJobs/Program.cs
@@ -9,4 +9,20 @@ namespace DemoJobs
             Console.WriteLine("Hello World!");
         }
     }
+
+    class Job
+    {
+        public string Description { get; set; }
+        public double Time { get; set; }
+        public double Rate { get; set; }
+        
+        public Job(string description, double time, double rate)
+        {
+            Description = description;
+            Time = time;
+            Rate = rate;
+        }
+
+        public Job() => new Job("", 0, 0);
+    }
 }
