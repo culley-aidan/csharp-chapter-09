aacffb4d7a609bc3c5066496e37548f85276baa6
culley-aidan <culley-aidan@users.noreply.github.com>
2019-12-02 08:25:46 -0600

    started

---
+++ ConferencesDemo/ConferencesDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConferencesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
